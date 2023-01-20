302b66b7c1d11f459e26bba96553954d2e019e73
lukkasz323 <39344794+lukkasz323@users.noreply.github.com>
2023-01-20 21:47:39 +0100

    Add Debug class

---
+++ ProcgenGame/Core/Debug.cs
@@ -0,0 +1,13 @@
+﻿namespace ProcgenGame.Core;
+
+public static class Debug
+{
+    public static dynamic A { get; set; }
+    public static dynamic B { get; set; }
+    public static dynamic C { get; set; }
+
+    public static void ExpensiveNothing(int operations = 9_000_000) 
+    {
+        for (int i = 0; i < operations; i++) ;
+    }
+}
