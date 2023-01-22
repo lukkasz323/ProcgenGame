8aa16e204cf7149315116b7adebeca674d0e162b
lukkasz323 <39344794+lukkasz323@users.noreply.github.com>
2023-01-22 23:02:08 +0100

    Add interfaces of components

---
+++ ProcgenGame/Core/IEntity.cs
@@ -0,0 +1,6 @@
+﻿namespace ProcgenGame.Core;
+
+public interface IEntity
+{
+    public int Id { get; set; }
+}
---
+++ ProcgenGame/Core/IPhysics.cs
@@ -0,0 +1,9 @@
+﻿namespace ProcgenGame.Core;
+
+public interface IPhysics : IEntity
+{
+    public Vector2 Position { get; set; }
+    public Vector2 Velocity { get; set; }
+    public int Size { get; set; }
+    public int Speed { get; set; }
+}
