ad633ae08d8d5dbafc0cdf4e7146d7cba919ac42
lukkasz323 <39344794+lukkasz323@users.noreply.github.com>
2023-01-28 08:03:37 +0100

    Add editor settings / Change GlobalUsings

--- ProcgenGame/GlobalUsings.cs
+++ ProcgenGame/GlobalUsings.cs
@@ -1,5 +1,6 @@
 ﻿global using Microsoft.Xna.Framework;
 global using Microsoft.Xna.Framework.Graphics;
+global using Microsoft.Xna.Framework.Input;
 global using System;
 global using System.Collections.Generic;
 global using System.Linq;
