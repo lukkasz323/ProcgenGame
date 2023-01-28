ea8d4c99f5be36ad60de71f07053eca0836f101d
Taco (タコス) <65432314+tacosontitan@users.noreply.github.com>
2023-01-28 11:15:53 -0600

    Added global usings for threading and DI support.

--- ProcgenGame/GlobalUsings.cs
+++ ProcgenGame/GlobalUsings.cs
@@ -3,3 +3,8 @@ global using Microsoft.Xna.Framework.Graphics;
 global using System;
 global using System.Collections.Generic;
 global using System.Linq;
+global using System.Threading;
+global using System.Threading.Tasks;
+global using Microsoft.Extensions.Hosting;
+global using Microsoft.Extensions.Logging;
+global using Microsoft.Extensions.DependencyInjection;
