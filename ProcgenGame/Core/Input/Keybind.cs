e49f9aa27ed14ea108722e7313965b50df222fc7
Taco (タコス) <65432314+tacosontitan@users.noreply.github.com>
2023-01-28 14:50:30 -0600

    Established keybind model.

--- ProcgenGame/Core/Input/Keybind.cs
+++ ProcgenGame/Core/Input/Keybind.cs
@@ -1,12 +1,28 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Input;
 
-namespace ProcgenGame.Core.Input
+namespace ProcgenGame.Core.Input;
+
+/// <summary>
+/// Represents a keybinding in the game.
+/// </summary>
+public class Keybind
 {
-    public class Keybind
+    /// <summary>
+    /// The display name for the keybind.
+    /// </summary>
+    public string Name { get; set; }
+    /// <summary>
+    /// The keys required to activate the keybind.
+    /// </summary>
+    public IEnumerable<Keys> Keys { get; set; }
+    /// <summary>
+    /// Creates a new keybind.
+    /// </summary>
+    /// <param name="name">The display name for the keybind.</param>
+    /// <param name="keys">The keys required to activate the keybind.</param>
+    public Keybind(string name, params Keys[] keys)
     {
-        
+        Name = name;
+        Keys = keys;
     }
 }
