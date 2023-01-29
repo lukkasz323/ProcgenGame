61c00fe8dcfc367eecd4e049a8520bb3669371c6
Taco (タコス) <65432314+tacosontitan@users.noreply.github.com>
2023-01-28 19:53:36 -0600

    Updated input options to use proper model.

--- ProcgenGame/Core/Input/InputOptions.cs
+++ ProcgenGame/Core/Input/InputOptions.cs
@@ -10,19 +10,19 @@ public class InputOptions
     /// <summary>
     /// The bindings of keys to actions in the game.
     /// </summary>
-    public Dictionary<KeybindAction, IEnumerable<Keys>> Bindings { get; set; }
+    public Dictionary<InputAction, IEnumerable<Keys>> Bindings { get; set; }
     /// <summary>
     /// Creates a new <see cref="InputOptions"/> instance with the default configuration.
     /// </summary>
     public InputOptions()
     {
-        Bindings = new Dictionary<KeybindAction, IEnumerable<Keys>> {
-            { KeybindAction.Close, new Keys[] { Keys.Escape } },
-            { KeybindAction.ToggleFullscreen, new Keys[] { Keys.LeftAlt, Keys.Enter } },
-            { KeybindAction.MoveUp, new Keys[] { Keys.W } },
-            { KeybindAction.MoveDown, new Keys[] { Keys.S } },
-            { KeybindAction.MoveLeft, new Keys[] { Keys.A } },
-            { KeybindAction.MoveRight, new Keys[] { Keys.D } }
+        Bindings = new Dictionary<InputAction, IEnumerable<Keys>> {
+            { InputAction.Close, new Keys[] { Keys.Escape } },
+            { InputAction.ToggleFullscreen, new Keys[] { Keys.LeftAlt, Keys.Enter } },
+            { InputAction.MoveUp, new Keys[] { Keys.W } },
+            { InputAction.MoveDown, new Keys[] { Keys.S } },
+            { InputAction.MoveLeft, new Keys[] { Keys.A } },
+            { InputAction.MoveRight, new Keys[] { Keys.D } }
         };
     }
 }
