af77f442812cf18fc07e0d97cfa9028fe2161f40
Taco (タコス) <65432314+tacosontitan@users.noreply.github.com>
2023-01-28 19:17:34 -0600

    Added code analysis directive.

--- ProcgenGame/GlobalUsings.cs
+++ ProcgenGame/GlobalUsings.cs
@@ -2,6 +2,7 @@
 global using Microsoft.Xna.Framework.Graphics;
 global using System;
 global using System.Collections.Generic;
+global using System.Diagnostics.CodeAnalysis;
 global using System.Linq;
 global using System.Threading;
 global using System.Threading.Tasks;
