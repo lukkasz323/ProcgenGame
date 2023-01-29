7ff9a890befddbe4baa1a0179cb13a5ff307d109
Taco (タコス) <65432314+tacosontitan@users.noreply.github.com>
2023-01-29 07:45:12 -0600

    Added mediator support to reduce coupling.

--- ProcgenGame/Core/Input/InputProcessor.cs
+++ ProcgenGame/Core/Input/InputProcessor.cs
@@ -1,28 +1,29 @@
 using Microsoft.Xna.Framework.Input;
+using ProcgenGame.Core.Rendering;
 
 namespace ProcgenGame.Core.Input;
+
+#nullable enable
 
 /// <summary>
 /// Represents an <see cref="IFrameProcessor"/> that handles player input.
 /// </summary>
 public class InputProcessor : IFrameProcessor
 {
-    private readonly Game _game;
-    private readonly GraphicsDeviceManager _graphics;
+    private readonly IMediator _mediator;
     private readonly InputOptions _options;
-    private readonly Dictionary<InputAction?, bool> _actionLocks;
+    private readonly Dictionary<InputAction, bool> _actionLocks;
     /// <summary>
     /// Creates a new <see cref="InputProcessor"/> instance.
     /// </summary>
     /// <param name="options">The <see cref="InputOptions"/> utilized to configure the instance.</param>
     /// <param name="game">The <see cref="Game"/> instance that should be utilized to handle exit requests.</param>
     /// <param name="graphics">The <see cref="GraphicsDeviceManager"/> instance that should be utilized to handle fullscreen requests.</param>
-    public InputProcessor(InputOptions options, Game game, GraphicsDeviceManager graphics)
+    public InputProcessor(InputOptions options, IMediator mediator)
     {
-        _game = game;
         _options = options;
-        _graphics = graphics;
-        _actionLocks = new Dictionary<InputAction?, bool>();
+        _mediator = mediator;
+        _actionLocks = new Dictionary<InputAction, bool>();
         foreach (InputAction action in Enum.GetValues<InputAction>())
             _ = _actionLocks.TryAdd(action, false);
     }
@@ -33,23 +34,18 @@ public class InputProcessor : IFrameProcessor
         if (!TryDetermineAction(out InputAction? requestedAction))
             return;
 
-        try
-        {
-            // Lock the requested action.
-            _actionLocks[requestedAction] = false;
+        // If the requested action is already being handled, ignore the request.
+        InputAction action = requestedAction ?? throw new InvalidOperationException("The requested action was null.");
+        if (_actionLocks[action])
+            return;
 
-            // Handle special actions.
-            switch (requestedAction)
-            {
-                case InputAction.Close: _game.Exit(); return;
-                case InputAction.ToggleFullscreen: _graphics.ToggleFullScreen(); return;
-            }
+        // Handle the requested action.
+        if (!TryCreateInputNotification(action, out INotification? requestedNotification))
+            return;
 
-            // TODO: Handle player interactions.
-        } finally
-        {
-            _actionLocks[requestedAction] = false;
-        }
+        // Publish the notification.
+        INotification notification = requestedNotification ?? throw new InvalidOperationException("The requested notification was null.");
+        _ = _mediator.Publish(notification, CancellationToken.None);
     }
     /// <summary>
     /// Attempts to determine the action that should be performed based on the pressed keys.
@@ -71,6 +67,30 @@ public class InputProcessor : IFrameProcessor
         }
 
         requestedAction = null;
+        return false;
+    }
+    private bool TryCreateInputNotification(InputAction action, [NotNullWhen(true)]  out INotification? notification)
+    {
+        try
+        {
+            // Lock the requested action.
+            _actionLocks[action] = false;
+
+            // Handle special actions.
+            switch (action)
+            {
+                case InputAction.Close: notification = new InputNotification<Game>(action); return true;
+                case InputAction.ToggleFullscreen: notification = new InputNotification<RenderingProcessor>(action); return true;
+            }
+
+            // TODO: Handle player interactions.
+
+        } finally
+        {
+            _actionLocks[action] = false;
+        }
+
+        notification = null;
         return false;
     }
 }
