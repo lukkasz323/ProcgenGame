f5465cef3668074fd22d8031f5f828a72fd17486
Taco (タコス) <65432314+tacosontitan@users.noreply.github.com>
2023-02-05 12:57:20 -0600

    Added `AddGame` extension method.

--- ProcgenGame/Core/IServiceCollectionExtensions.cs
+++ ProcgenGame/Core/IServiceCollectionExtensions.cs
@@ -11,5 +11,14 @@ internal static class IServiceCollectionExtensions
     /// <param name="services">The service contract for registering dependencies with the DI container.</param>
     /// <returns>The service contract for registering dependencies with the DI container.</returns>
     internal static IServiceCollection AddProcgenGame(this IServiceCollection services) =>
-        services.AddHostedService<GameService<Game1>>();
+        services.AddGame<Game1>();
+    /// <summary>
+    /// Adds a <see cref="Game"/> to the service contract.
+    /// </summary>
+    /// <typeparam name="T">Specifies the concrete <see cref="Game"/> implementation to add.</typeparam>
+    /// <param name="services">The service contract for registering dependencies with the DI container.</param>
+    /// <returns>The service contract for registering dependencies with the DI container.</returns>
+    internal static IServiceCollection AddGame<T>(this IServiceCollection services) where T : Game =>
+        services.AddSingleton<T>()
+                .AddHostedService<GameService<T>>();
 }
