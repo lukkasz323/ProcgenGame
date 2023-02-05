6149822f4f1e136725bc72ca138988d92da9bcc3
Taco (タコス) <65432314+tacosontitan@users.noreply.github.com>
2023-02-05 12:40:41 -0600

    Added and configured host to run the game with DI enabled.

--- ProcgenGame/Program.cs
+++ ProcgenGame/Program.cs
@@ -1,4 +1,15 @@
-﻿
-using var game = new ProcgenGame.Core.Game1();
+﻿using ProcgenGame.Core;
 
-game.Run();
+// Create a host and start running it.
+using IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services => services
+        .AddLogging(BuildLogging)
+        .AddProcgenGame()
+).Build();
+await host.RunAsync();
+
+// Builds the logging service.
+static void BuildLogging(ILoggingBuilder logging) =>
+    logging.ClearProviders()
+           .AddDebug()
+           .SetMinimumLevel(LogLevel.Debug);
