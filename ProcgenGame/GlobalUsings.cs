2816950730e67b6aedf813da39215181602e2658
Taco (タコス) <65432314+tacosontitan@users.noreply.github.com>
2023-02-05 12:27:27 -0600

    Added global usings to promote dependency injection.

--- ProcgenGame/GlobalUsings.cs
+++ ProcgenGame/GlobalUsings.cs
@@ -6,3 +6,7 @@ global using System.Collections;
 global using System.Collections.Generic;
 global using System.Linq;
 global using System.Threading;
+
+global using Microsoft.Extensions.Hosting;
+global using Microsoft.Extensions.Logging;
+global using Microsoft.Extensions.DependencyInjection;
